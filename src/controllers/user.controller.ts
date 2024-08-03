3c273832e1daf0a349248af644cf88a7086c17f5
vuongpm-0516 <pham.minh.vuong@sun-asterisk.com>
2024-08-04 04:09:56 +0700

    View list of instructors/students

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,11 +1,34 @@
 import { NextFunction, Response, Request } from 'express';
 import asyncHandler from 'express-async-handler';
+import * as userService from '../services/user.service';
 import { users, courseRecommends } from '../mock/data';
 import { UserRole } from '../enums/UserRole';
 
 export const getUserList = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
     res.render('users/index', { users });
+  }
+);
+
+export const getInstructorList = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const instructors = await userService.getInstructorList();
+    res.render('instructors/list', {
+      title: req.t('title.list_instructor'),
+      instructors,
+      currentPath: req.baseUrl + req.path,
+    });
+  }
+);
+
+export const getStudentList = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const students = await userService.getStudentList();
+    res.render('students/list', {
+      title: req.t('title.list_student'),
+      students,
+      currentPath: req.baseUrl + req.path,
+    });
   }
 );
 
---
+++ src/services/user.service.ts
@@ -0,0 +1,19 @@
+import { AppDataSource } from '../config/data-source';
+import { User } from '../entity/user.entity';
+import { UserRole } from '../enums/UserRole';
+
+const userRepository = AppDataSource.getRepository(User);
+
+export const getInstructorList = async () => {
+  return userRepository.find({
+    order: { username: 'ASC' },
+    where: { role: UserRole.INSTRUCTOR },
+  });
+};
+
+export const getStudentList = async () => {
+  return userRepository.find({
+    order: { username: 'ASC' },
+    where: { role: UserRole.STUDENT },
+  });
+};
