6128aaf1ca98c969c898caad50d826da32d82560
vuongpm-0516 <pham.minh.vuong@sun-asterisk.com>
2024-08-04 14:43:05 +0700

    Config eslint-plugin-pug and format all file *.pug

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,10 +4,11 @@ import tseslint from 'typescript-eslint';
 import eslintPluginPrettier from 'eslint-plugin-prettier';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+import pugPlugin from 'eslint-plugin-pug';
 import { fixupConfigRules } from '@eslint/compat';
 
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx,pug}'] },
   { ignores: ['src/public/**', 'test/**', 'node_modules/**'] },
   { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
   { languageOptions: { globals: globals.browser } },
@@ -18,6 +19,7 @@ export default [
   {
     plugins: {
       prettier: eslintPluginPrettier,
+      pug: pugPlugin,
     },
     settings: {
       react: {
@@ -28,5 +30,9 @@ export default [
       'prettier/prettier': 'error',
       '@typescript-eslint/no-unused-vars': 'off',
     },
+  },
+  {
+    files: ['**/*.pug'],
+    processor: pugPlugin.processors['.pug'],
   },
 ];
